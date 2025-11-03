99060626618ee06f1783c5980c1a581549bfc364
FrontendTF <t.feyerl@gmail.com>
2025-11-03 18:37:35 +0100

    create project

---
+++ canadianbeer.py
@@ -0,0 +1,10 @@
+import pandas
+import matplotlib
+import scipy
+import statsmodels
+import numpy
+import seaborn
+import sklearn
+import ISLP
+
+print("Alle Libraries wurden erfolgreich importiert ")
